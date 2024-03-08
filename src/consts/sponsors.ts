40ac2fad49dafd0c9ffeaf28ed30082bee9ae252
vadelgado <vadelgado@udenar.edu.co>
2024-03-08 17:15:43 -0500

    Logos Acompañantes

---
+++ src/consts/sponsors.ts
@@ -0,0 +1,32 @@
+export const SPONSORS = [
+	{
+		id: "logoFunes",
+		name: "Alcaldia Municipal de Funes",
+		url: "https://www.funes-narino.gov.co/",
+	},
+	{
+		id: "jamez",
+		name: "jamezJamez Cundar Zambrano",
+		url: "https://www.facebook.com/kike.gomez.5680",
+	},
+	{
+		id: "losPaisitas",
+		name: "Corporación Deportiva Los Paisitas de Medellín",
+		url: "https://festivaldefestivales.com/",
+	},
+	{
+		id: "futurasEstrellas",
+		name: "Escuela de Fútbol Futuras Estrellas Corporacion Social y Deportiva",
+		url: "https://www.facebook.com/oscararmando.futurasestrellas",
+	},
+	{
+		id: "alianzaSurena",
+		name: "Organización y montaje eventos deportivos, culturales y artisticos Alianza Sureña. ",
+		url: "https://www.facebook.com/oscarbolanoscastro",
+	},
+	{
+		id: "osbol",
+		name: "Osbol Sport Manager",
+		url: "https://www.facebook.com/profile.php?id=853397289",
+	},
+] as const
