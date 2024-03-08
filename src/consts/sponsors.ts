cb21be2c2ea1b69a7818ed82a193f0dd3cb3ac08
vadelgado <vadelgado@udenar.edu.co>
2024-03-08 17:30:42 -0500

    fiz resolucion img

--- src/consts/sponsors.ts
+++ src/consts/sponsors.ts
@@ -3,30 +3,37 @@ export const SPONSORS = [
 		id: "logoFunes",
 		name: "Alcaldia Municipal de Funes",
 		url: "https://www.funes-narino.gov.co/",
+		w:40,
+
 	},
 	{
 		id: "jamez",
 		name: "jamezJamez Cundar Zambrano",
 		url: "https://www.facebook.com/kike.gomez.5680",
+		w:44,
 	},
 	{
 		id: "losPaisitas",
 		name: "Corporación Deportiva Los Paisitas de Medellín",
 		url: "https://festivaldefestivales.com/",
+		w:40,
 	},
 	{
 		id: "futurasEstrellas",
 		name: "Escuela de Fútbol Futuras Estrellas Corporacion Social y Deportiva",
 		url: "https://www.facebook.com/oscararmando.futurasestrellas",
+		w:24,
 	},
 	{
 		id: "alianzaSurena",
 		name: "Organización y montaje eventos deportivos, culturales y artisticos Alianza Sureña. ",
 		url: "https://www.facebook.com/oscarbolanoscastro",
+		w:40,
 	},
 	{
 		id: "osbol",
 		name: "Osbol Sport Manager",
 		url: "https://www.facebook.com/profile.php?id=853397289",
+		w:40,
 	},
 ] as const
