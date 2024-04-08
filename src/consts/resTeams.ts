25b336249b6ba398470b557b177195fba11b3842
vadelgado <vadelgado@udenar.edu.co>
2024-04-08 14:13:40 -0500

    Tabla de Grupos 17

--- src/consts/resTeams.ts
+++ src/consts/resTeams.ts
@@ -2,82 +2,82 @@ export const RES_TEAMS = [
     {
         id: "1",
         name: "Alcaldía de Funes - Fundación Fénix",        
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "2",
         name: "Alcaldía de Pupiales",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "3",
         name: "Américo Quiñones La Unión",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "4",
         name: "Asociación Deportivo Pasto",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "5",
         name: "Club Atlético Sucre Ipiales",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "6",
         name: "Club CAR Nariño",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "7",
         name: "Club Eder Ruales Ipiales",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "8",
         name: "Club Estudiantes Pasto",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "9",
         name: "Club Futuras Estrellas Nariño",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "10",
         name: "Club Leones FC Pasto",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "11",
         name: "Club MPB Ricaurte",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "12",
         name: "Club Sporting de Nariño",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "13",
         name: "Club Sueño Fútbol Cumbal",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "14",
         name: "Club Talentos del Sur Ipiales",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "15",
         name: "Escuela Deportivo Pasto 2013",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     {
         id: "16",
         name: "Escuela William Andrade Mora Samaniego",
-        resultadoSorteo:0
+        resultadoSorteo:17
     },
     
 ] as const;
