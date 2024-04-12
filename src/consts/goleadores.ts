361b038dd55a7c9c6980da134c23a11cd51068fc
vadelgado <vadelgado@udenar.edu.co>
2024-04-12 15:56:28 -0500

    Update table positions and group names in various components

---
+++ src/consts/goleadores.ts
@@ -0,0 +1,66 @@
+export const RES_GOLEADORES = [
+  {
+    id: "1",
+    name: "Alcaldía Funes - Fénix.",
+  },
+  {
+    id: "16",
+    name: "Alcaldía de Pupiales.",
+  },
+  {
+    id: "15",
+    name: "Américo Quiñones Unión.",
+  },
+  {
+    id: "14",
+    name: "Asoc. Dep. Pasto.",
+  },
+  {
+    id: "13",
+    name: "Atlético Sucre Ipiales.",
+  },
+  {
+    id: "12",
+    name: "CAR Nariño.",
+  },
+  {
+    id: "11",
+    name: "Eder Ruales Ipiales.",
+  },
+  {
+    id: "10",
+    name: "Estudiantes Pasto.",
+  },
+  {
+    id: "9",
+    name: "Fut. Estrellas Nariño.",
+  },
+  {
+    id: "8",
+    name: "Leones FC Pasto.",
+  },
+  {
+    id: "7",
+    name: "MPB Ricaurte.",
+  },
+  {
+    id: "6",
+    name: "Sporting de Nariño.",
+  },
+  {
+    id: "5",
+    name: "Sueño Fútbol Cumbal.",
+  },
+  {
+    id: "4",
+    name: "Talentos Sur Ipiales.",
+  },
+  {
+    id: "3",
+    name: "Esc. Dep. Pasto '13.",
+  },
+  {
+    id: "2",
+    name: "Esc. W. A. Mora Samaniego.",
+  },
+] as const;
